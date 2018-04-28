2a936c39d50adb171c9387fc3f74c4a1590c89fb
Rares <raresboza@gmail.com>
2018-04-28 04:31:24 -0700

    model userRegister

---
+++ DMT/UserRegister.swift
@@ -0,0 +1,26 @@
+//
+//  UserRegister.swift
+//  DMT
+//
+//  Created by Boza Rares-Dorian on 28/04/2018.
+//  Copyright © 2018 Boggy. All rights reserved.
+//
+
+import Foundation
+struct UserRegister: Codable{
+    let nume: String
+    let prenume: String
+    let email: String
+    let parola: String
+    let telefon: String
+    let tipUser: String
+    
+    private enum CodingKeys: String, CodingKey{
+        case nume
+        case prenume
+        case email
+        case parola
+        case telefon = "nr_telefon"
+        case tipUser = "tip_user"
+    }
+}
